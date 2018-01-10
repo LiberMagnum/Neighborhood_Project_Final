7d83200b72463978af2c31a9e143f662491a9511
LiberMagnum <30744369+LiberMagnum@users.noreply.github.com>
2018-01-10 15:47:20 +0900

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,22 +1,29 @@
 var locations = [
 	{
 		name: "Maeda Point",
-		position: {lat: 26.4450618, lng: 127.7714252}
+		position: {lat: 26.4450618, lng: 127.7714252},
+		difficulty: 'medium'
 	},
 	{
 		name: "Kadena Marina",
-		position: {lat: 26.351209, lng: 127.745474}
+		position: {lat: 26.351209, lng: 127.745474},
+		difficulty: 'easy'
 	},
 	{
 		name: "Sunabe Seawall",
-		position: {lat: 26.335189, lng: 127.745531}
+		position: {lat: 26.335189, lng: 127.745531},
+		difficulty: 'easy'
 	},
 	{
 		name: "Cape Zanpa",
-		position: {lat: 26.440311, lng: 127.714792}
+		position: {lat: 26.440311, lng: 127.714792},
+		difficulty: 'difficult'
 	},
 	{
 		name: "Cape Hedo",
-		position: {lat: 26.872511, lng: 128.264687}
+		position: {lat: 26.872511, lng: 128.264687},
+		difficulty: 'difficult'
 	}
 ];
+
+var possibleDifficulties = ['easy', 'medium', 'difficult', 'expert'];
